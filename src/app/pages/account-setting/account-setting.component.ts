87a329d1ea79908a38ac8ef2bfd72eea0e431f85
jheyson zurita <jheysoria20@gmail.com>
2020-08-16 10:38:34 -0400

    Servicios de settings

--- src/app/pages/account-setting/account-setting.component.ts
+++ src/app/pages/account-setting/account-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-account-setting',
@@ -8,33 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingComponent implements OnInit {
   public linkTheme = document.querySelector('#theme');
-  public links: NodeListOf<Element>;
 
-  constructor() { }
+  constructor(
+    private settingsService: SettingsService
+  ) { }
 
   ngOnInit(): void {
-    this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
+    const links = document.querySelectorAll('.selector');
+    this.settingsService.checkCurrentTheme(links);
   }
 
   changeTheme(theme: string) {
-    const url = `./assets/css/colors/${theme}.css`;
-
-    this.linkTheme.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    this.checkCurrentTheme();
+    this.settingsService.changeTheme(theme);
   }
 
-  checkCurrentTheme() {
-    this.links.forEach(elem => {
-      elem.classList.remove('working');
-      const btnTheme = elem.getAttribute('data-theme')
-      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-      const currentTheme = this.linkTheme.getAttribute('href');
-      if (btnThemeUrl == currentTheme) {
-        elem.classList.add('working');
-      }
-    });
-  }
 
 }
